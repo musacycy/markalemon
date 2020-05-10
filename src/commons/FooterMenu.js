9588a4f4786caaa750251676195d3ab4ffe19eb1
Musa Cycy <musacycy@gmail.com>
2020-05-10 04:47:48 +0300

    some changes applied on FooterMenu

--- src/commons/FooterMenu.js
+++ src/commons/FooterMenu.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHome, faSearch, faUser,faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
-import { Footer, FooterTab} from 'native-base';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-
  class FooterMenu extends Component {
 
     state = {
@@ -11,49 +8,25 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
         
       }
         clickEvent = () => {
-     
+
+          
        this.setState({
      
          isClick : !this.state.isClick
        })
+       
      }
 
     render() {
-        const {isClick} = this.state;
+        const {isClick} = this.state
         return (
-            <Footer>
-            <FooterTab style={{marginHorizontal:20, justifyContent: 'space-between', alignItems:'center'}}>
-              <TouchableOpacity key='home' onPress={this.clickEvent}>
-                {
-                  isClick ? <FontAwesomeIcon name='home' icon={faHome} size={32} size={32}></FontAwesomeIcon> :
-                            <FontAwesomeIcon name='home' icon={faHome} size={32} color='orange'></FontAwesomeIcon> 
-                }
-  
-              </TouchableOpacity>
-              <TouchableOpacity key='search' onPress={this.clickEvent}>
-                {
-                  isClick ? <FontAwesomeIcon name='search' icon={faSearch} size={32}></FontAwesomeIcon> :
-                            <FontAwesomeIcon name='search' icon={faSearch} size={32} color='orange'></FontAwesomeIcon> 
-                }
-  
-              </TouchableOpacity>
-              <TouchableOpacity onPress={this.clickEvent}>
-                {
-                  isClick ? <FontAwesomeIcon name='basket'icon={faShoppingBasket} size={32}></FontAwesomeIcon> :
-                                  <FontAwesomeIcon name='basket'icon={faShoppingBasket} size={32} color='orange'></FontAwesomeIcon> 
-                }
-  
-              </TouchableOpacity>
-              <TouchableOpacity onPress={this.clickEvent}>
-                {
-                  isClick ? <FontAwesomeIcon name='account' icon={faUser} size={32}></FontAwesomeIcon> :
-                                  <FontAwesomeIcon name='account' icon={faUser} size={32} color='orange'></FontAwesomeIcon>
-                }
-  
-              </TouchableOpacity>
-              
-            </FooterTab>
-          </Footer>
+          <TouchableOpacity onPress={this.clickEvent} >
+          {
+            isClick ? <FontAwesomeIcon  icon={this.props.icon} size={this.props.size}></FontAwesomeIcon> :
+                      <FontAwesomeIcon  icon={this.props.icon} size={this.props.size} color={this.props.color} ></FontAwesomeIcon> 
+          }
+
+        </TouchableOpacity>
         )
     }
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import {Image, Dimensions} from 'react-native';
-import { Container, Content,Form,Item,Label,Input} from 'native-base';
+import { faHome, faSearch, faUser,faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
+
+import { Container,Footer,FooterTab, Content,Form,Item,Label,Input} from 'native-base';
 import SubmitButton from '../commons/SubmitButton.js';
 import FbLogin from '../commons/FbLogin';
 import GoogleLogin from '../commons/GoogleLogin';
+import FooterMenu from '../commons/FooterMenu';
 const {width, height} = Dimensions.get('window');
+
 export default class LoginPage extends Component {
   render() {
     return (
@@ -25,10 +30,18 @@ export default class LoginPage extends Component {
             <GoogleLogin Text='Login with Google'></GoogleLogin>
          
           </Form>
-          
-
+         
         </Content>
-      
+        <Footer>
+            <FooterTab style={{marginHorizontal:20, justifyContent: 'space-between', alignItems:'center'}}>
+           
+              <FooterMenu icon={faHome} size={24} color="orange" ></FooterMenu>
+              <FooterMenu icon={faSearch} size={24} color="orange" ></FooterMenu>
+              <FooterMenu icon={faShoppingBasket} size={24} color="orange" ></FooterMenu>
+              <FooterMenu icon={faUser} size={24} color="orange"></FooterMenu>
+            </FooterTab>
+          </Footer>
+
       </Container>
     );
   }
